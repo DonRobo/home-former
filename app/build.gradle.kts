bc450a113b57df1d91f5da1bed4b7212620e273f
Robert Tieber <robert.tieber@gmail.com>
2024-06-10 21:22:26 +0200

    Publish distribution zip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     alias(libs.plugins.jvm)
     application
+    `maven-publish`
 }
+
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -38,4 +41,52 @@ application {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        val gprUser = (project.findProperty("gpr.user") as String? ?: System.getenv("GPR_USER"))?.ifBlank { null }
+        if (gprUser != null) {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/DonRobo/home-former")
+                credentials {
+                    username = gprUser
+                    password =
+                        (project.findProperty("gpr.key") as String? ?: System.getenv("GPR_TOKEN"))?.ifBlank { null }
+                            ?: error("No GitHub token set")
+                }
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("distribution") {
+            val ghRef = System.getenv("GH_REF")
+            val versionToUse = when {
+                ghRef == null -> project.version.toString()
+                ghRef.startsWith("refs/heads/") -> ghRef.removePrefix("refs/heads/") + "-SNAPSHOT"
+                ghRef.startsWith("refs/tags/") -> ghRef.removePrefix("refs/tags/")
+                else -> error("Unknown GH_REF: $ghRef")
+            }
+            groupId = "at.robert.home-former"
+            artifactId = "hf-cli"
+            version = versionToUse
+
+            artifact(tasks.distZip)
+        }
+    }
+}
+
+distributions {
+    main {
+        distributionBaseName = "hf-cli"
+    }
+}
+
+tasks.jar {
+    archiveBaseName = "hf-cli"
+}
+
+tasks.startScripts {
+    applicationName = "hf"
 }
