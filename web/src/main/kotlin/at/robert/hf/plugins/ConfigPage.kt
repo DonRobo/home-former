fe8357982631a6cb05a6a55a8b842f399af0f8bf
Robert Tieber <robert.tieber@gmail.com>
2024-06-21 23:07:23 +0200

    Render config file

--- web/src/main/kotlin/at/robert/hf/plugins/ConfigPage.kt
+++ web/src/main/kotlin/at/robert/hf/plugins/ConfigPage.kt
@@ -1,6 +1,11 @@
 package at.robert.hf.plugins
 
 import at.robert.hf.ConfigFiles
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.IntNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.TextNode
+import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.html.*
@@ -9,57 +14,130 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
 
-fun Application.configureConfigPage() {
-    routing {
-        get("/config") {
-            call.respondHtmlBody("Config") {
-                h1 {
-                    +"Available Configs"
-                }
-                val configs = ConfigFiles.getConfigs()
-                if (configs.isEmpty()) {
-                    +"No configs available"
-                } else {
-                    ul {
-                        configs.forEach { config ->
-                            li {
-                                +config
-                            }
+private fun BODY.configList() {
+    main {
+        h1 {
+            +"Available Configs"
+        }
+        val configs = ConfigFiles.getConfigs()
+        if (configs.isEmpty()) {
+            +"No configs available"
+        } else {
+            ul {
+                configs.forEach { config ->
+                    li {
+                        a(href = "/config/$config") {
+                            +config
                         }
                     }
                 }
-                form(action = "/config", method = FormMethod.post, encType = FormEncType.multipartFormData) {
-                    h2 {
-                        +"Upload Config"
-                    }
-                    input(type = InputType.file, name = "file") {
-                        accept = "text/yaml"
-                    }
-                    input(type = InputType.submit) {
-                        value = "Upload"
-                    }
+            }
+        }
+        form(action = "/config", method = FormMethod.post, encType = FormEncType.multipartFormData) {
+            h2 {
+                +"Upload Config"
+            }
+            input(type = InputType.file, name = "file") {
+                accept = "text/yaml"
+            }
+            input(type = InputType.submit) {
+                value = "Upload"
+            }
 
+        }
+    }
+}
+
+private suspend fun uploadConfigFile(call: ApplicationCall) {
+    val formParams = call.receiveMultipart()
+    formParams.forEachPart { part ->
+        when (part) {
+            is PartData.FileItem -> {
+                ConfigFiles.uploadConfig(
+                    part.originalFileName!!,
+                    part.streamProvider().bufferedReader().use { it.readText() }
+                )
+            }
+
+            else -> {
+                println("Unexpected part type: ${part.javaClass}")
+            }
+        }
+        part.dispose()
+    }
+    call.respondRedirect("/config")
+}
+
+private suspend fun specificConfigPage(call: ApplicationCall) {
+    val name = call.parameters["name"] ?: return call.respondText(
+        "Missing name",
+        status = HttpStatusCode.BadRequest
+    )
+
+    val config = ConfigFiles.getConfig(name)
+
+    call.respondHtmlBody("Config - $name") {
+        main {
+            h1 {
+                +"Config $name"
+            }
+            div {
+                config.states.forEach { state ->
+                    h2 {
+                        +"${state.provider.name} - ${state.provider.config}"
+                    }
+                    renderJson(state.state)
                 }
             }
         }
-        post("/config") {
-            val formParams = call.receiveMultipart()
-            formParams.forEachPart { part ->
-                when (part) {
-                    is PartData.FileItem -> {
-                        ConfigFiles.uploadConfig(
-                            part.originalFileName!!,
-                            part.streamProvider().bufferedReader().use { it.readText() }
-                        )
-                    }
+    }
+}
 
-                    else -> {
-                        println("Unexpected part type: ${part.javaClass}")
+private fun FlowContent.renderJson(json: JsonNode) {
+    when (json) {
+        is ObjectNode -> {
+            json.fieldNames().forEach {
+                val fieldName = it
+                val fieldValue = json.get(fieldName)
+                div {
+                    attributes["style"] = "margin-left: 1rem;"
+                    h3 {
+                        +fieldName
                     }
+                    renderJson(fieldValue)
                 }
-                part.dispose()
             }
-            call.respondRedirect("/config")
+        }
+
+        is IntNode -> div {
+            attributes["style"] = "margin-left: 1rem;"
+            +json.intValue().toString()
+        }
+
+        is TextNode -> div {
+            attributes["style"] = "margin-left: 1rem;"
+            +json.textValue()
+        }
+
+        else -> div {
+            attributes["style"] = "margin-left: 1rem;"
+            +"Unknown type: ${json.javaClass.simpleName}"
+            br
+            +json.toString()
+        }
+    }
+}
+
+fun Application.configureConfigPage() {
+    routing {
+        get("/config") {
+            call.respondHtmlBody("Config", BODY::configList)
+        }
+        post("/config") {
+            uploadConfigFile(call)
+        }
+        get("/config/{name}") {
+            specificConfigPage(call)
         }
     }
 }
@@ -68,6 +146,7 @@ private suspend fun ApplicationCall.respondHtmlBody(title: String, block: BODY.(
     this.respondHtml {
         head {
             title(title)
+            link(rel = "stylesheet", href = "https://unpkg.com/missing.css@1.1.2")
         }
         body {
             block()
