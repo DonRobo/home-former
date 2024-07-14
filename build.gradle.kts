dfe19639b16754d5f6ee90bab0c5cbe588101695
Robert Tieber <robert.tieber@gmail.com>
2024-07-14 21:47:49 +0200

    Add release workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,43 @@ allprojects {
         }
     }
 }
+
+task("setVersion") {
+    doLast {
+        val versionToSet = project.findProperty("newVersion") as String
+        val thisFile = File("build.gradle.kts")
+        var versionRegex = Regex("(\\s*)version\\s*=\\s*\"\\d+\\.\\d+\\.\\d+\"\\s*")
+        var count = 0
+        val content = thisFile.readLines()
+            .map { line ->
+                val match = versionRegex.matchEntire(line)
+                if (match != null) {
+                    count++
+                    "${match.groupValues[1]}version = \"$versionToSet\""
+                } else {
+                    line
+                }
+            }
+        require(count == 1) {
+            "Expected to find exactly one version line in '${thisFile.name}', found $count"
+        }
+        thisFile.writeText(content.joinToString("\n"))
+
+        versionRegex = Regex("(\\s*)version\\s*:\\s*\"\\d+\\.\\d+\\.\\d+\"\\s*")
+        val configYamlFile = File("home-assistant-addon/config.yaml")
+        count = 0
+        val configYamlContent = configYamlFile.readLines().map {
+            val match = versionRegex.matchEntire(it)
+            if (match != null) {
+                count++
+                "${match.groupValues[1]}version: \"$versionToSet\""
+            } else {
+                it
+            }
+        }
+        require(count == 1) {
+            "Expected to find exactly one version line in '${configYamlFile.name}', found $count"
+        }
+        configYamlFile.writeText(configYamlContent.joinToString("\n"))
+    }
+}
