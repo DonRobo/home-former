61367ba883013cc1e0c80b72d7be9be198e6626d
Robert Tieber <robert.tieber@gmail.com>
2024-07-21 20:45:24 +0200

    Fix build

--- web/src/main/kotlin/at/robert/hf/Application.kt
+++ web/src/main/kotlin/at/robert/hf/Application.kt
@@ -1,9 +1,9 @@
 package at.robert.hf
 
+import at.robert.hf.config.configureErrors
+import at.robert.hf.config.configureStaticContent
 import at.robert.hf.page.configureConfigPage
-import at.robert.hf.page.configureErrors
 import at.robert.hf.page.configureShellyManager
-import at.robert.hf.page.configureStaticContent
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
